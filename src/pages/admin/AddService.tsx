2c6ff053a115a4136558eacaa83789b4c257b3d9
Abu <abujakaria316@gmail.com>
2024-01-28 03:46:48 +0600

    invalidate cache

--- src/pages/admin/AddService.tsx
+++ src/pages/admin/AddService.tsx
@@ -1,7 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { FormEvent, useState } from "react";
+
 const AddService = () => {
+  const [serviceName, setServiceName] = useState("");
+
+  const { mutateAsync, isError, isSuccess } = useMutation({
+    mutationFn: async (data) => {
+      return await fetch("http://localhost:5000/api/v1/services", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    },
+  });
+  console.log({ isError, isSuccess });
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const serviceData = {
+      name: serviceName,
+      description: "Replaced down Battery",
+      devices: ["Smartphone", "Laptop", "Tablet"],
+      price: 300.0,
+    };
+    await mutateAsync(serviceData);
+    console.log("posted");
+  };
   return (
     <div>
-      <h1>AddService</h1>
+      <form
+        className="mt-10 flex justify-center items-center "
+        onSubmit={handleFormSubmit}
+      >
+        <div className="flex flex-col w-96 gap-5 ">
+          {" "}
+          <input
+            type="text"
+            name=""
+            id=""
+            className="border py-2 rounded-sm focus:outline-none px-3"
+            onChange={(e) => setServiceName(e.target.value)}
+          />
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,5 +1,4 @@
-import { getServices } from "@/api/admin/service/service.api";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   Table,
   TableBody,
@@ -13,9 +12,49 @@ import Container from "@/components/Container";
 import { Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useGetService } from "@/api/admin/service/service.hook";
+import { FormEvent, useState } from "react";
 
 const ServiceList = () => {
   const { data: services, isLoading, isError } = useGetService();
+
+  //Start
+
+  // InvalidateCache or Realtime data update
+
+  const [serviceName, setServiceName] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync, isSuccess } = useMutation({
+    mutationFn: async (data) => {
+      return await fetch("http://localhost:5000/api/v1/services", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["services"] });
+    },
+  });
+  console.log({ isError, isSuccess });
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const serviceData = {
+      name: serviceName,
+      description: "Replaced down Battery",
+      devices: ["Smartphone", "Laptop", "Tablet"],
+      price: 300.0,
+    };
+    await mutateAsync(serviceData);
+    console.log("posted");
+  };
+
+  //End
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -57,6 +96,23 @@ const ServiceList = () => {
           </TableRow>
         </TableFooter>
       </Table>
+
+      <form
+        className="mt-10 flex justify-center items-center "
+        onSubmit={handleFormSubmit}
+      >
+        <div className="flex flex-col w-96 gap-5 ">
+          {" "}
+          <input
+            type="text"
+            name=""
+            id=""
+            className="border py-2 rounded-sm focus:outline-none px-3"
+            onChange={(e) => setServiceName(e.target.value)}
+          />
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
     </Container>
   );
 };
